27804a131654b1d897dd82a5007ae8cb8414b5ea
penKih <vladlen110110@gmail.com>
2026-02-18 16:05:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('History of GPUs loaded');
+
+const btn=document.querySelector('.toggle');
+btn.onclick=()=>{
+ document.body.classList.toggle('light');
+ localStorage.setItem('theme',
+ document.body.classList.contains('light')?'light':'dark');
+}
+window.onload=()=>{
+ if(localStorage.getItem('theme')==='light')
+ document.body.classList.add('light');
+}
