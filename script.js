81d7313d7eb20108465dff07845c746930e81702
penKih <vladlen110110@gmail.com>
2026-02-18 16:25:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const btn = document.querySelector('.toggle');
+const icon = document.querySelector('#theme-icon');
 
 btn.onclick = () => {
   document.body.classList.toggle('light');
-  localStorage.setItem(
-    'theme',
-    document.body.classList.contains('light') ? 'light' : 'dark'
-  );
+  if(document.body.classList.contains('light')){
+    icon.textContent='🌞';
+    localStorage.setItem('theme','light');
+  } else {
+    icon.textContent='🌙';
+    localStorage.setItem('theme','dark');
+  }
 };
 
 window.onload = () => {
-  if(localStorage.getItem('theme') === 'light'){
+  if(localStorage.getItem('theme')==='light'){
     document.body.classList.add('light');
+    icon.textContent='🌞';
+  } else {
+    icon.textContent='🌙';
   }
 };
