7a0a6c4caf3b115a0b8cefb4209e742c88f02e0a
penKih <vladlen110110@gmail.com>
2026-02-18 16:11:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+const btn = document.querySelector('.toggle');
 
-const btn=document.querySelector('.toggle');
-btn.onclick=()=>{
- document.body.classList.toggle('light');
- localStorage.setItem('theme',
- document.body.classList.contains('light')?'light':'dark');
-}
-window.onload=()=>{
- if(localStorage.getItem('theme')==='light')
- document.body.classList.add('light');
-}
+btn.onclick = () => {
+  document.body.classList.toggle('light');
+  localStorage.setItem(
+    'theme',
+    document.body.classList.contains('light') ? 'light' : 'dark'
+  );
+};
+
+window.onload = () => {
+  if(localStorage.getItem('theme') === 'light'){
+    document.body.classList.add('light');
+  }
+};
